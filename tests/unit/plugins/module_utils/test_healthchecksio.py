c00ffcd8f648ee03a2aa192cb04067085bf30c9e
Mark Mercado <mamercad@gmail.com>
2021-09-13 21:21:28 -0400

    Stubbing out unit tests

---
+++ tests/unit/plugins/module_utils/test_healthchecksio.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from argparse import ArgumentParser
+
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible_collections.community.general.tests.unit.compat.mock import patch
+
+from ansible.errors import AnsibleError
+from ansible.module_utils import six
+
+from ansible_collections.community.healthchecksio.plugins.module_utils.healthchecksio import (
+    HealthchecksioHelper,
+)
+
+
+class TestHealthchecksioModuleUtil:
+    def test_healthchecksio(self):
+        assert 1 == 1
---
+++ tests/unit/plugins/modules/test_ping.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from argparse import ArgumentParser
+
+from ansible_collections.community.general.tests.unit.compat import unittest
+from ansible_collections.community.general.tests.unit.compat.mock import patch
+
+from ansible.errors import AnsibleError
+from ansible.module_utils import six
+
+from ansible_collections.community.healthchecksio.plugins.module_utils.healthchecksio import (
+    HealthchecksioHelper,
+    Ping,
+)
+
+
+class TestPingPlugin:
+    def test_ping(self):
+        assert 1 == 1
