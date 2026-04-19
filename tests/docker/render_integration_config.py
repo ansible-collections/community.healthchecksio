99bd714e651af74f30fc423743bf85c0db128a96
mamerbot <mamerbot@gmail.com>
2026-04-19 12:24:58 -0400

    style: format render_integration_config.py with black

--- tests/docker/render_integration_config.py
+++ tests/docker/render_integration_config.py
@@ -6,25 +6,25 @@ from pathlib import Path
 
 
 def main() -> None:
-    api_key = Path('/tmp/hc_ci_key.txt').read_text(encoding='utf-8').strip()
-    host = os.environ.get('HC_TEST_HOST', '127.0.0.1').strip()
-    content = Path('tests/integration/integration_config.yml.template').read_text(
-        encoding='utf-8'
+    api_key = Path("/tmp/hc_ci_key.txt").read_text(encoding="utf-8").strip()
+    host = os.environ.get("HC_TEST_HOST", "127.0.0.1").strip()
+    content = Path("tests/integration/integration_config.yml.template").read_text(
+        encoding="utf-8"
     )
     content = content.replace(
-        '${MANAGEMENT_API_TOKEN:-$HEALTHCHECKSIO_API_TOKEN}', api_key
+        "${MANAGEMENT_API_TOKEN:-$HEALTHCHECKSIO_API_TOKEN}", api_key
     )
     content = content.replace(
         '${MANAGEMENT_API_BASE_URL:-"https://healthchecks.io/api/v1"}',
-        f'http://{host}:8000/api/v1',
+        f"http://{host}:8000/api/v1",
     )
     content = content.replace(
         '${PING_API_BASE_URL:-"https://hc-ping.com"}',
-        f'http://{host}:8000/ping',
+        f"http://{host}:8000/ping",
     )
-    content = content.replace('${PING_API_TOKEN:-""}', '')
-    Path('tests/integration/integration_config.yml').write_text(
-        content, encoding='utf-8'
+    content = content.replace('${PING_API_TOKEN:-""}', "")
+    Path("tests/integration/integration_config.yml").write_text(
+        content, encoding="utf-8"
     )
 
 
