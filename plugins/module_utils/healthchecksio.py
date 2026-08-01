fd218e5e6a3ef8e7597e4c9fd2148aac071c603d
Mark Mercado <mamercad@gmail.com>
2026-08-01 08:45:16 -0400

    fix: handle invalid JSON on legacy Python

--- plugins/module_utils/healthchecksio.py
+++ plugins/module_utils/healthchecksio.py
@@ -31,7 +31,7 @@ class Response(object):
                 try:
                     json_body = json.loads(to_text(self.info["body"]))
                     return json_body
-                except json.decoder.JSONDecodeError:
+                except ValueError:
                     return {}
             return None
         try:
